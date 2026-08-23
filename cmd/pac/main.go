dcb1c8a70add6eebe728bc953104e9684a5a0ae1
tech <tech@local>
2026-08-23 12:43:52 +0500

    add cmd

---
+++ cmd/pac/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/darakcheeff/pac/internal/storage"
+	"github.com/darakcheeff/pac/internal/ui"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func main() {
+	dbPath := flag.String("db", "", "Path to SQLite database file")
+	flag.Parse()
+
+	gtk.Init(nil)
+
+	store, err := storage.NewStore(*dbPath)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+	app, err := ui.NewAppWindow(store)
+	if err != nil {
+		log.Fatalf("Failed to create application window: %v", err)
+	}
+
+	// Trap termination signals (Ctrl+C, SIGTERM, SIGHUP) to save session state on exit
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	go func() {
+		<-sigChan
+		glib.IdleAdd(func() {
+			app.Quit()
+		})
+	}()
+
+	app.Window.ShowAll()
+
+	fmt.Println("PAC Connection Manager NextGen started successfully.")
+	gtk.Main()
+}
